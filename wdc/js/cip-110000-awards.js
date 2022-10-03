8083b2a757a711e7f2465462f22def9076700c24
MMenezes <mmenezes@Mmenezes-MacBook-Pro.local>
2022-10-03 18:23:23 -0400

    add institution data - still work in progress

--- wdc/js/cip-110000-awards.js
+++ wdc/js/cip-110000-awards.js
@@ -1,5 +1,9 @@
 
 let savedCSVData; // Always a string
+
+// Schema for Institution/College
+institution_column_names = ["unitid_year", "unitid", "year", "inst_name", "state_abbr", "zip", "county_fips",
+        "region", "cbsa", "congress_district_id", "inst_control", "institution_level", "hbcu", "tribal_college", "city"]
 // Create the connector object
 var myConnector = tableau.makeConnector();
 console.log("1");
@@ -29,73 +33,220 @@ console.log("2");
 // Define the schema
 myConnector.getSchema = async function(schemaCallback) {
 
-    console.time("Creating table schema");
-    let conData = JSON.parse(tableau.connectionData);
-    console.log(`check conData: ${conData.yearValue}`);
-    let dataUrl = conData.dataUrl;
-    console.log(`old url: ${dataUrl}`);
-    let yearValue = conData.yearValue;
-    console.log(`Year value: ${yearValue}`);
-    let method = conData.method;
-    let delimiter =
-        conData.delimiter && conData.delimiter !== "" ? conData.delimiter : ",";
-    let encoding =
-        conData.encoding && conData.encoding !== "" ? conData.encoding : "";
-    let token = tableau.password;
-    let mode = conData.fastMode ? "fast" : conData.mode ? conData.mode : "typed";
-    // console.log(`check url:${finalUrl}`)
-    //Automate the csv link based on the selected years
-    let dataUrlPrefix = "https://educationdata.urban.org/csv/ipeds/colleges_ipeds_completions-2digcip_";
-    // let dataUrlPrefix = dataUrl.slice(0, 77)
-    let extension = ".csv";
-    let yearinit = yearValue[0];
-    let finalUrl = `${dataUrlPrefix}${yearinit}${extension}`;
-    console.log(`Prefix: ${dataUrlPrefix}`);
-    console.log(`which year: ${yearValue}`);
-    console.log(typeof yearValue);
-    console.log(yearValue[0]);
-    console.log(yearValue[1]);
-    console.log(yearValue[2]);
-    console.log(`Data Url Final: ${finalUrl}`);
-    let data =
-        savedCSVData ||
-        (await _retrieveCSVData({ finalUrl, method, token, encoding }));
-    console.log(data);
-    let cols = [];
-    
-    if (mode === "fast") {
-        let headers = data
-        .split(/\r?\n/)[0]
-        .split(delimiter)
-        .map(header => header.replace(/"/g, ""));
-        headers = _sanitizeKeys(headers);
-        for (let header in headers) {
-        cols.push({
-            id: header,
-            alias: headers[header].alias,
-            dataType: "string"
+    // Standard connection specifies pre-joined tables
+        var standardConnection = {
+            "alias": "Joined awards data",
+            "tables": [{
+                "id": "awards",
+                "alias": "CIP 11 Awards"
+            }, {
+                "id": "institution",
+                "alias": "Institution"
+            }],
+            "joins": [{
+                "left": {
+                    "tableAlias": "CIP 11 Awards",
+                    "columnId": "unitid_year"
+                },
+                "right": {
+                    "tableAlias": "Institution",
+                    "columnId": "unitid_year"
+                },
+                "joinType": "left"
+            }]
+        };
+
+
+        var var_description = {};
+        var var_label = {};
+        var variable_metadata = await fetch('https://educationdata-stg.urban.org/api/v1/api-variables/?mode=tableauwdc')
+            .then(response => response.json());
+        var variable_metadata_feat = variable_metadata.results;
+        variable_metadata_feat.forEach(function (arrayItem) {
+           var_description[arrayItem.variable] = arrayItem.description;
+           var_label[arrayItem.variable] = arrayItem.label;
         });
-        }
-    } else {
-        let headers = _determineTypes(_parse(data, delimiter, true));
-        console.log(headers)
-        for (let field in headers) {
-        cols.push({
-            id: field,
-            alias: headers[field].alias,
-            dataType: headers[field].dataType
-        });
-        }
-    }
-    
-    let tableSchema = {
-        id: "csvData",
-        alias: "CSV Data",
-        columns: cols
-    };
-    
-    console.timeEnd("Creating table schema");
-    schemaCallback([tableSchema]);
+
+        // Schema for Completion
+        let completionCols = [
+            {
+                id: "unitid_year",
+                alias: "ID-year",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "unitid",
+                alias: var_label["unitid"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["unitid"]
+            },
+            {
+                id: "year",
+                alias: var_label["year"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["year"]
+            },
+            {
+                id: "cipcode",
+                alias: var_label["cipcode"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["cipcode"]
+            },
+            {
+                id: "award_level",
+                alias: var_label["award_level"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["award_level"]
+            },
+            {
+                id: "majornum",
+                alias: var_label["majornum"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["majornum"]
+            },
+            {
+                id: "sex",
+                alias: var_label["sex"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["sex"]
+            },
+            {
+                id: "race",
+                alias: var_label["race"],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description["race"]
+            },
+            {
+                id: "awards",
+                alias: var_label["awards"],
+                dataType: tableau.dataTypeEnum.int,
+                description: var_description["awards"]
+            }
+        ];
+
+        let completionTable = {
+            id: "awards",
+            alias: "CIP 11 Awards",
+            columns: completionCols
+        };
+
+        // Schema for Institution/College
+        let institutionCols = [
+            {
+                // unitid_year
+                id: institution_column_names[0],
+                alias: "ID-year",
+                dataType: tableau.dataTypeEnum.string,
+            },
+            {
+                // unitid
+                id: institution_column_names[1],
+                alias: var_label[institution_column_names[1]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[1]]
+            },
+            {
+                // year
+                id: institution_column_names[2],
+                alias: var_label[institution_column_names[2]],
+                dataType: tableau.dataTypeEnum.date,
+                description: var_description[institution_column_names[2]]
+            },
+            {
+                // inst_name
+                id: institution_column_names[3],
+                alias: var_label[institution_column_names[3]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[3]]
+            },
+            {
+                // state_abbr
+                id: institution_column_names[4],
+                alias: var_label[institution_column_names[4]],
+                dataType: tableau.dataTypeEnum.string,
+                geoRole: tableau.geographicRoleEnum.state_province,
+                description: var_description[institution_column_names[4]]
+            },
+            {
+                // zip
+                id: institution_column_names[5],
+                alias: var_label[institution_column_names[5]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[5]]
+            },
+            {
+                // county_fips
+                id: institution_column_names[6],
+                alias: var_label[institution_column_names[6]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[6]]
+            },
+            {
+                // region
+                id: institution_column_names[7],
+                alias: var_label[institution_column_names[7]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[7]]
+            },
+            {
+                // cbsa
+                id: institution_column_names[8],
+                alias: var_label[institution_column_names[8]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[8]]
+            },
+            {
+                // congress_district_id
+                id: institution_column_names[9],
+                alias: "114th congressional district identification number",
+                dataType: tableau.dataTypeEnum.string,
+                geoRole: tableau.geographicRoleEnum.congressional_district,
+                description: var_description[institution_column_names[9]]
+            },
+            {
+                // inst_control
+                id: institution_column_names[10],
+                alias: var_label[institution_column_names[10]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[10]]
+            },
+            {
+                // institution_level
+                id: institution_column_names[11],
+                alias: var_label[institution_column_names[11]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[11]]
+            },
+            {
+                // hbcu
+                id: institution_column_names[12],
+                alias: var_label[institution_column_names[12]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[12]]
+            },
+            {
+                // tribal_college
+                id: institution_column_names[13],
+                alias: var_label[institution_column_names[13]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[13]]
+            },
+            {
+                // city
+                id: institution_column_names[14],
+                alias: var_label[institution_column_names[14]],
+                dataType: tableau.dataTypeEnum.string,
+                description: var_description[institution_column_names[14]]
+            }
+        ];
+
+        let institutionTable = {
+            id: "institution",
+            alias: "Institution",
+            columns: institutionCols
+        };
+        //schemaCallback([completionTable, institutionTable]);
+        schemaCallback([completionTable, institutionTable], [standardConnection]);
 };
 console.log("3");
 // Download the data
@@ -112,51 +263,121 @@ myConnector.getData = async function(table, doneCallback){
           conData.encoding && conData.encoding !== "" ? conData.encoding : "";
       let token = tableau.password;
       let mode = conData.fastMode ? "fast" : conData.mode ? conData.mode : "typed";
-    //creating the csv url
-    let dataUrlPrefix = "https://educationdata.urban.org/csv/ipeds/colleges_ipeds_completions-2digcip_";
-    let dataUrlExtension = ".csv";
-    // what is this for?
-    let tableSchemas = [];
-    var all_rows = [];
-    for (let i = 0; i < yearArray.length; i++) {
-      console.time("Getting data");
-      let yearValue = yearArray[i];
-      console.log(`Pull data year ${yearValue}`);
-      let finalUrl = `${dataUrlPrefix}${yearValue}${dataUrlExtension}`;
-      console.log(finalUrl)
-      //data = savedCSVData || (await _retrieveCSVData({ finalUrl, method, token, encoding }));
-      data = await _retrieveCSVData({ finalUrl, method, token, encoding });
+      let row_index = 0;
+      let size = 10000;
+    switch(table.tableInfo.id) {
+        case 'awards':
+            //creating the csv url
+            let dataUrlPrefix = "https://educationdata.urban.org/csv/ipeds/colleges_ipeds_completions-2digcip_";
+            let dataUrlExtension = ".csv";
+            // what is this for?
+            var all_rows = [];
+            for (let i = 0; i < yearArray.length; i++) {
+              console.time("Getting awards data");
+              let yearValue = yearArray[i];
+              console.log(`Pull data year ${yearValue}`);
+              let finalUrl = `${dataUrlPrefix}${yearValue}${dataUrlExtension}`;
+              console.log(finalUrl)
+              //data = savedCSVData || (await _retrieveCSVData({ finalUrl, method, token, encoding }));
+              data = await _retrieveCSVData({ finalUrl, method, token, encoding });
 
-      switch (mode) {
-        case "fast":
-        rows = _parse(data, delimiter, false).slice(1);
-        break;
-        case "typed":
-        rows = _cleanData(_parse(data, delimiter, true));
-        break;
-        case "loosetyped":
-        rows = _parse(data, delimiter, true).slice(1);
-        break;
-        default:
-        rows = _cleanData(_parse(data, delimiter, true));
-        }
+              switch (mode) {
+                case "fast":
+                rows = _parse(data, delimiter, false).slice(1);
+                break;
+                case "typed":
+                rows = _cleanData(_parse(data, delimiter, true));
+                break;
+                case "loosetyped":
+                rows = _parse(data, delimiter, true).slice(1);
+                break;
+                default:
+                rows = _cleanData(_parse(data, delimiter, true));
+                }
 
-        console.log(`Rows length ${rows.length}`);
-        all_rows = all_rows.concat(rows);
+                console.log(`Rows length ${rows.length}`);
+                all_rows = all_rows.concat(rows);
 
-    }
-    console.log(`Combined length ${all_rows.length}`);
+            }
+            console.log(`Combined length ${all_rows.length}`);
 
-    let row_index = 0;
-    let size = 10000;
-    while (row_index < all_rows.length) {
-        table.appendRows(all_rows.slice(row_index, size + row_index));
-        row_index += size;
-        tableau.reportProgress("Getting row: " + row_index);
-    }
-    console.timeEnd("Getting data");
-    
-    doneCallback();
+            //let row_index = 0;
+            //let size = 10000;
+            while (row_index < all_rows.length) {
+                table.appendRows(all_rows.slice(row_index, size + row_index));
+                row_index += size;
+                tableau.reportProgress("Getting row: " + row_index);
+            }
+            console.timeEnd("Getting awards data");
+
+            doneCallback();
+            break;
+        case 'institution':
+            let finalUrl = "https://educationdata.urban.org/csv/ipeds/colleges_ipeds_directory.csv";
+            console.log(finalUrl)
+            console.time("Getting institution data");
+            console.log(finalUrl)
+            data = savedCSVData || (await _retrieveCSVData({ finalUrl, method, token, encoding }));
+            data = await _retrieveCSVData({ finalUrl, method, token, encoding });
+            console.log(data)
+            console.log(mode)
+
+            switch (mode) {
+              case "fast":
+              rows = _parse(data, delimiter, false).slice(1);
+              break;
+              case "typed":
+              rows = _cleanData(_parse(data, delimiter, true));
+              break;
+              case "loosetyped":
+              parsed_data = _parse(data, delimiter, true)
+              rows = parsed_data.slice(1);
+              header = parsed_data[0];
+              break;
+              default:
+              rows = _cleanData(_parse(data, delimiter, true));
+              }
+
+            console.log(header)
+            console.log(header)
+            var headerLength = header.length;
+            institutionColIndexes = [];
+            header_col_count = 0;
+            for (var i = 0; i < headerLength; i++) {
+                if(institution_column_names.includes(header[i])){
+                    //console.log(header[i]);
+                    institutionColIndexes.push(i);
+                    if (header[i] ==  "year"){
+                        year_index = i;
+                    }
+                }
+            }
+
+            rows_subset = []
+            rows.forEach((element) => {
+              row_year = element[year_index].toString()
+              console.log(row_year);
+              console.log(yearArray)
+              if (yearArray.includes(element[row_year])){
+                  console.log("IN ARRAY INDEX")
+                  row_subset = institutionColIndexes.map(i => element[i]);
+                  row_subset.unshift(row_subset[0] + '-' + row_subset[1]);
+                  rows_subset.push(row_subset);
+                  }
+            });
+
+
+            console.log(institutionColIndexes)
+
+            while (row_index < rows_subset.length) {
+                table.appendRows(rows_subset.slice(row_index, size + row_index));
+                row_index += size;
+                tableau.reportProgress("Getting row: " + row_index);
+            }
+            console.timeEnd("Getting institution data");
+            doneCallback();
+            break;
+    };
 };
 console.log("4");
 tableau.connectionName = "CSV Data";
